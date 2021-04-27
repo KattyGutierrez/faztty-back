f5c57ca3d823d48ce6a1885a28e3b59e6b995a55
KattyGutierrez <62726842+KattyGutierrez@users.noreply.github.com>
2021-04-27 14:14:10 -0500

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,7 @@
 
 insert into `tipo_negocio` (id, nombre, descripcion) values (1,'Bodega','Productos del día a día. Diferentes tipos de producto');
 insert into `tipo_negocio` (id, nombre, descripcion) values (2,'Dulcería','Productos de consumo rápido. Dulces, snacks, etc.');
-
+insert into `tipo_negocio` (id, nombre, descripcion) values (3,'Panadería','Productos relacionados con repostería y afines.');
 
 insert into `usuario` (tipo, id, email, password, dni,first_name, last_name) values ('C',1, 'rosa@unmsm.com','$2a$10$xBPpxqLBdUXblMGkCwadr.hsNVyJk04A3hb4Fcjn79YeYzAtYCZzC', 'kstrauss0', 'Rosa', 'Strauss');
 insert into `usuario` (tipo, id, email, password, dni,first_name, last_name) values ('C',2, 'cfeldstern1@ucoz.ru', '$2a$10$xBPpxqLBdUXblMGkCwadr.hsNVyJk04A3hb4Fcjn79YeYzAtYCZzC', 'ekornel3','Elliott', 'Kornel');
@@ -15,6 +15,7 @@ insert into `usuario` (tipo, id, email, password, ruc, nombre,direccion,imagen,p
 insert into `categoria` (id, nombre, descripcion) values (1,'Lacteo','Productos derivados de la leche');
 insert into `categoria` (id, nombre, descripcion) values (2,'Galleta Dulce','Galletas empaquetadas de sabores dulces');
 insert into `categoria` (id, nombre, descripcion) values (3,'Snack','Snacks empaquetadas, pequeños o grandes');
+insert into `categoria` (id, nombre, descripcion) values (4,'Bebidas','Productos bebibles que incluye gaseosas, refrescos, etc');
 
 insert into `producto` (id, nombre, marca,precio,puntuacion,id_categoria,id_negocio) values (1,'Tarro de Leche Gloria Azul','Gloria', 3.50, 78,1,3 );
 insert into `producto` (id, nombre, marca,precio,puntuacion,id_categoria,id_negocio) values (2,'Tarro de Leche Gloria Roja','Gloria', 3.80, 75,1,3 );
